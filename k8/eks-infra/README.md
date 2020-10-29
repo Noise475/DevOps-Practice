8effdf8b68942313e6630713e99ed4aca19a62cb
Noise475 <61090208+Noise475@users.noreply.github.com>
2020-10-29 16:17:24 -0400

    EKS service example (#8)
    This commit will encompass two examples:
    
    1. Minikube
    
    - A cluster spin up through minikube to link the rpc-app to publish metrics to promethus using kubernetes.
    
    2. EKS
    
    - A cluster spin up through AWS EKS using terraform.
    
    Minikube:
    - Create rbac.yml for ServiceAccount, ClusterRole and ClusterRoleBinding manifests for Prometheus monitoring deploy
    
    - Create Prometheus service, and deploy manifests
    
    - Create rpc-app service and deploy manifests
    
    EKS:
    
    create terraform scripts/modules for infrastructure:
    - eks-cluster.tf; Module to define the cluster config for EKS
    
    - k8.rback.yml; creates service account, and clusterRoleBinding for EKS cluster
    
    - kuberentes.tf; initializes kubernetes provider & sets cluster ca cert
    
    - outputs.tf; Output EKS cluster info after creation
    
    - security-groups; Set up AWS IAM permissions for EKS
    
    - version.tf; Define and guarantee versions for providors
    
    - vpc.tf; create vpc, public/private subnets, and nat gateway for EKS
    cluster
    
    TODO:
    - Deploy and link 2 clusters

---
+++ k8/eks-infra/README.md
@@ -0,0 +1,29 @@
+# Setup EKS using Terraform
+
+This example assumes you have [credentials](https://github.com/Noise475/DevOps-Practice/blob/master/terraform/README.md#terraform) for aws setup.
+
+To run this example do the following:
+
+1. clone the repository
+
+``` shell
+git clone git@github.com:Noise475/DevOps-Practice.git
+```
+
+2. cd into eks-infra directory
+
+``` shell
+cd DevOps-Practice/k8/eks-infra
+```
+
+3. run terraform init
+
+``` shell
+terraform init
+```
+
+4. run terraform apply
+
+``` shell
+terraform apply
+```
---
+++ k8/eks-infra/security-groups.tf
@@ -0,0 +1,42 @@
+resource "aws_security_group" "k8_mgmt_one" {
+  name_prefix = "k8_mgmt_one"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["10.0.0.0/8", ]
+  }
+
+  
+}
+
+resource "aws_security_group" "k8_mgmt_two" {
+  name_prefix = "k8_mgmt_two"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["192.168.0.0/16", ]
+  }
+
+  
+}
+
+resource "aws_security_group" "k8_all_mgmt" {
+  name_prefix = "k8_all_mgmt"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = ["10.0.0.0/8",
+      "172.16.0.0/12",
+    "192.168.0.0/16"]
+  }
+}
